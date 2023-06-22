df76f44cb7edc815bcd579cfd11039b2e0df1436
ErikLaN68 <eriklaneave@gmail.com>
2023-06-21 23:29:25 -0600

    Got the basic outline of the ui set in the file called ui.c

---
+++ src/ui.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+  puts("Welcome to Tokenizer");
+  
+  while (1){
+    
+    puts("Enter a string to be tokenized, type quit to end the program, or enter history to see all pervious entered strings.\n>");
+
+    break;
+  }
+  return 0;
+}
