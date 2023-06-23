ab78711f003c5aac119d5a1306420e3418e655b0
ErikLaN68 <eriklaneave@gmail.com>
2023-06-23 13:23:29 -0600

    Added the make run function to the Makefile. Cleaned up ui.c and got strcomp working with history and exit. ui.c is done and need to start working on the tokenizer

--- src/ui.c
+++ src/ui.c
@@ -1,25 +1,45 @@
 #include "stdlib.h"
 #include "stdio.h"
 int myStrcmp (char*,char*);
+//Need to re-order these but they are used in the main below. Could put into a include file
+#define MAX 100
+// \n is needed on these because fgets adds a \n to the user input when getting it.
+char *pHis = "history\n";
+char *pExit = "exit\n";
 
 int main(int argc, char **argv)
 {
+  printf("The program name is <%s>\n", *(argv+0));
   puts("Welcome to the Tokenizer");
-  printf("The program name is <%s>\n\n", *(argv+0));
-  while (1){
-    fputs("Enter a string to be tokenized, enter history to see pervious inputs, or enter quit to leave the program\n> ",stdout);
+  fputs("Enter a string to be tokenized\n--COMMANDS--\nEnter history to see pervious inputs\nEnter exit to end the program\n",stdout);
+  // makes the userString to take input
+  char userString[MAX];
+  
+  while (1) {
     
-    char userString[100];
+    fputs("> ",stdout);
     fgets(userString,sizeof(userString),stdin);
-    printf("%s\n", userString);
+    printf("echo %s", userString);
+    
+    // pointer that points to the user input
+    char *pUser = userString;
 
-    char *pUserString = userString;
-    char *pHistory = "history";
-    if (myStrcmp(pUserString,pHistory)) {
-	printf("while print history");
+    // compares user input to "history" to view old inputs
+    if (myStrcmp(pUser,pHis) == 0) {
+	printf("will print history\n");
+      }
+
+    // compares user input to "exit" to leave the program
+    else if (myStrcmp(pUser,pExit) == 0) {
+      puts("Thanks for using the program");
+      goto done;
       }
     
-    break;
+    else {
+      puts("Will be tokenized");
+    }
+    
   }
-  return 0;
+  done:
+    return 0;
 }
