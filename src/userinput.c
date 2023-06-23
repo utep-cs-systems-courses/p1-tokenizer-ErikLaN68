7790c9a302ca78e4e7ff213b27467b4432ebe2f2
ErikLaN68 <eriklaneave@gmail.com>
2023-06-23 00:02:47 -0600

    Added strcpy and strcmp to the userinput.c file. Need to put in include file to be used in the main called ui.c

--- src/userinput.c
+++ src/userinput.c
@@ -1,1 +1,22 @@
 /* WIll have all functions that are needed for the ui and userinput part of the program */
+
+/* Copies the string in s to t */
+void strcpy(char *s, char *t)
+{
+  while ((*s = *t) != '\0') {
+    s++;
+    t++;
+  }
+}
+
+/* Compares string s to string t and returns 0 if s==t, <0 s<t, or >0 s>t */
+int strcmp (char *s, char *t)
+{
+  while (*s == *t) {
+    s++;
+    t++;
+    if (*s == '/0')
+      return 0;
+  }
+  return *s - *t;
+}
